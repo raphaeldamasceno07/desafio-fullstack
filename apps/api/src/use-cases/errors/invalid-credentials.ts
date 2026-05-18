ba4f1ed8d7acb6ae16232b7a541232d1397136e9
raphaeldamasceno07 <raphael_damasceno07@icloud.com>
2026-05-18 11:51:19 -0300

    feat(auth): create auth use case test  case and error

---
+++ apps/api/src/use-cases/errors/invalid-credentials.ts
@@ -0,0 +1,7 @@
+import { AppError } from './app-error'
+
+export class InvalidCredentialsError extends AppError {
+  constructor() {
+    super('Invalid credentials.', 401)
+  }
+}
---
+++ apps/api/src/use-cases/session/authenticate.ts
@@ -0,0 +1,38 @@
+import { IHashProvider } from '@/providers/HashProvider/IHashProvider'
+import { InMemoryUserRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { User } from 'generated/prisma/client'
+import { InvalidCredentialsError } from '../errors/invalid-credentials'
+
+interface AuthenticationUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticationUseCaseResponse {
+  user: User
+}
+
+export class AuthenticationUseCase {
+  constructor(
+    private userRepository: InMemoryUserRepository,
+    private hashProvider: IHashProvider,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticationUseCaseRequest): Promise<AuthenticationUseCaseResponse> {
+    const user = await this.userRepository.findByEmail(email)
+
+    if (!user) throw new InvalidCredentialsError()
+
+    const doesPasswordMatch = await this.hashProvider.compareHash(
+      password,
+      user.password_hash,
+    )
+
+    if (!doesPasswordMatch) throw new InvalidCredentialsError()
+
+    return { user }
+  }
+}
