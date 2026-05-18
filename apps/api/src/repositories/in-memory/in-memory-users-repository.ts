4d4e79956d886239937d533db196020be59501a2
raphaeldamasceno07 <raphael_damasceno07@icloud.com>
2026-05-18 09:54:39 -0300

    feat: create user repository interface, in-memory repository, and case error

---
+++ apps/api/src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,37 @@
+import { CreateUserRequest } from '@movie-challenge/core-types'
+import { User } from 'generated/prisma/client'
+import { UserRepository } from '../users-repository'
+
+export class InMemoryUserRepository implements UserRepository {
+  public items: User[] = []
+
+  async findById(id: string) {
+    const user = this.items.find(item => item.id === id)
+
+    if (!user) return null
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = this.items.find(item => item.email === email)
+
+    if (!user) return null
+
+    return user
+  }
+
+  async create(data: CreateUserRequest) {
+    const newUser = {
+      id: crypto.randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      createdAt: new Date(),
+    }
+
+    this.items.push(newUser)
+
+    return newUser
+  }
+}
---
+++ apps/api/src/use-cases/user/register.ts
@@ -0,0 +1,41 @@
+import { IHashProvider } from '@/providers/HashProvider/IHashProvider'
+import { UserRepository } from '@/repositories/users-repository'
+import { User } from 'generated/prisma/client'
+import { UserAlreadyExistsError } from '../errors/user-already-exists'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+interface RegisterUseCaseResponse {
+  user: User
+}
+
+export class RegisterUserUseCase {
+  constructor(
+    private usersRepository: UserRepository,
+    private hashProvider: IHashProvider,
+  ) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    const emailExists = await this.usersRepository.findByEmail(email)
+
+    const password_hash = await this.hashProvider.generateHash(password)
+
+    if (emailExists) throw new UserAlreadyExistsError()
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+
+    return { user }
+  }
+}
