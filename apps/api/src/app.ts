54b66987508e77db1d1cd1b2aa4eaccb2fa3d36a
raphaeldamasceno07 <raphael_damasceno07@icloud.com>
2026-05-20 18:27:32 -0300

    fix: fixed some error on app.ts

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,5 +1,6 @@
 import fastitfyCookie from '@fastify/cookie'
 import cors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
 import fastifyMultipart from '@fastify/multipart'
 import fastifySwagger from '@fastify/swagger'
 import scalarApiReference from '@scalar/fastify-api-reference'
@@ -18,17 +19,29 @@ import { AppError } from './use-cases/errors/app-error'
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>()
 
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastitfyCookie)
 app.register(fastifyMultipart, {
   limits: {
     fileSize: 5 * 1024 * 1024,
   },
 })
 
-app.setValidatorCompiler(validatorCompiler)
-app.setSerializerCompiler(serializerCompiler)
-
 app.register(cors, {
   origin: '*',
+})
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
 
 app.register(fastifySwagger, {
@@ -47,14 +60,12 @@ app.register(scalarApiReference, {
   configuration: {
     theme: 'kepler',
     layout: 'modern',
-    showSiderbar: true,
+    showSidebar: true,
   },
 })
 
 app.register(userRoutes)
 app.register(sessionRoutes)
-
-app.register(fastitfyCookie)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
@@ -83,8 +94,6 @@ app.setErrorHandler((error, _, reply) => {
 
   if (env.NODE_ENV !== 'prod') {
     console.error('❌ [Internal Error]:', error)
-  } else {
-    // TODO: Integrate with an error tracking service like Sentry or LogRocket
   }
 
   return reply.status(500).send({
