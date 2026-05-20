b70bb4ac47909ec6612ab5eaad104f8eefafd79f
raphaeldamasceno07 <raphael_damasceno07@icloud.com>
2026-05-20 10:54:47 -0300

    feat(user) created user profile route controller use case tests unit and e2e

--- apps/api/src/http/routes/userRoutes.ts
+++ apps/api/src/http/routes/userRoutes.ts
@@ -1,7 +1,9 @@
 import { app } from '@/app'
 import { registerBodySchema } from '@movie-challenge/core-types'
 import z from 'zod'
+import { profile } from '../controllers/users/profile'
 import { register } from '../controllers/users/register'
+import { verifyJWT } from '../middlewares/verify-jwt'
 
 export async function userRoutes() {
   app.post(
@@ -28,5 +30,32 @@ export async function userRoutes() {
     register,
   )
 
-  // app.get('/me', { onRequest: [verifyJWT], profile })
+  //precisa de autenticação para acessar as rotas abaixo
+  app.get(
+    '/me',
+    {
+      onRequest: [verifyJWT],
+      schema: {
+        tags: ['Users'],
+        summary: 'Get user profile',
+        description:
+          'Retrieve the profile information of the authenticated user',
+        response: {
+          200: z.object({
+            id: z.string().describe('User ID'),
+            name: z.string().describe('User name'),
+            email: z.string().describe('User email'),
+            createdAt: z.string().describe('Account creation date'),
+          }),
+          401: z.object({
+            message: z.string().describe('Unauthorized access'),
+          }),
+          404: z.object({
+            message: z.string().describe('Resource not found.'),
+          }),
+        },
+      },
+    },
+    profile,
+  )
 }
---
+++ apps/api/src/use-cases/user/get-user-profile.ts
@@ -0,0 +1,25 @@
+import { UserRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { ResourceNotFoundError } from '../errors/resource-not-found'
+
+interface GetUserProfileUseCaseRequest {
+  userId: string
+}
+
+interface GetUserProfileUseCaseResponse {
+  user: User
+}
+
+export class GetUserProfileUseCase {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute({
+    userId,
+  }: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseResponse> {
+    const user = await this.userRepository.findById(userId)
+
+    if (!user) throw new ResourceNotFoundError()
+
+    return { user }
+  }
+}
